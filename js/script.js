b6063a589b029022417e198ad5eba45ccdac5943
StarovoitKaterina <katrinblr1111@gmail.com>
2025-06-08 19:17:17 +0300

    js_start

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const MOCK_NOTES = [
+    {
+        id: 1,
+        title: 'Работа с формами',
+        content: 'К определённым полям формы можно обратиться через form.elements по значению, указанному в атрибуте name',
+        color: 'green',
+        isFavorite: false,
+    },
+]
+const colors = {
+    GREEN: 'green',
+    BLUE: 'blue',
+    RED: 'red',
+    YELLOW: 'yellow',
+    PURPLE: 'purple',
+}
+
+const model= {
+    notes: MOCK_NOTES
+}
+
+const view= {
+
+}
+
+const controller = {}
